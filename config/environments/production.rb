c5397fc9242ba9d1d975dd0da7d7142c8aee57de
vi92tvb <vi92tvb@gmail.com>
2025-04-09 15:36:54 +0700

    Update for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  config.hosts << ENV.fetch("APP_DOMAIN") { "shortlink-0bo8.onrender.com" }
+
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
--- docker/start.sh
+++ docker/start.sh
@@ -7,4 +7,11 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+# Run DB migrations
+echo "== Running DB Migrate =="
+bundle exec rails db:create
+bundle exec rails db:migrate
+
+# Start the Rails server
+echo "== Starting Rails server =="
 rails s -p 3000 -b '0.0.0.0'
